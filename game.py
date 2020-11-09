2413c9b40c988667a38a962a4455edadc8607e45
Marta <ga.marta.web@gmail.com>
2020-11-09 02:00:42 +0000

    correction

--- game.py
+++ game.py
@@ -8,24 +8,28 @@ def root():
     return render_template('newgame.html')
 
 
-@app.route('/admin/')
+@app.route('/admin/', methods=['POST', 'GET'])
 # for testing admin panel navigation - temporary directory
-# @app.route('/admin/<name>')
-def admin(name=None):
-    # templates word-game.html + admin.html body content
-    return render_template('admin.html')
+def admin():
+    if request.method == 'POST':
+        print(request.form)
+        name = request.form['email1']
+        # templates word-game.html + admin.html body content
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 
 # in test mode - admin panel directories - no credentials required yet
 @app.route('/admin/add/')
-def admin_add(name=None):
+def admin_add():
     # templates word-game.html + add.questions.html body content
     return render_template('add.question.html')
 
 
 # in test mode - admin panel directories - no credentials required yet
 @app.route('/admin/edit/')
-def admin_edit(name=None):
+def admin_edit():
     # templates word-game.html + edit.questions.html body content
     return render_template('edit.questions.html')
 
