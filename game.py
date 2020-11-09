bbd57b358de4e55835a4623f286010ed0f325fe4
Marta <ga.marta.web@gmail.com>
2020-11-09 00:39:12 +0000

    update .py

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
     return render_template('word-game.html')
 
+
 @app.route('/admin/')
 def admin():
     return render_template('admin.html')
-    
- if __name__ == ("__main__"):
+
+ if __name__ == "__main__":
      app.run(host='0.0.0.0', debug=True)
