49d8c14725a38138ee3ded3a687ff6da0595ada7
Marta <ga.marta.web@gmail.com>
2020-11-09 00:40:45 +0000

    .py

--- game.py
+++ game.py
@@ -11,5 +11,6 @@ def root():
 def admin():
     return render_template('admin.html')
 
- if __name__ == "__main__":
-     app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
