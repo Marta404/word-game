42c1e43d445df448b0d2c1f622c2e0f0e833f601
Marta <ga.marta.web@gmail.com>
2020-11-09 01:13:15 +0000

    correction

--- game.py
+++ game.py
@@ -3,16 +3,33 @@ app = Flask(__name__)
 
 
 @app.route('/')
-# for testing admin panel navigation - temporary directory
-@app.route('/<name>')
-def root(name=None):
-    # templates word-game.html + newgame.html
+def root():
+    # templates word-game.html + newgame.html body content
     return render_template('newgame.html')
 
 
 @app.route('/admin/')
-def admin():
+# for testing admin panel navigation - temporary directory
+# @app.route('/admin/<name>')
+def admin(name=None):
+    # templates word-game.html + admin.html body content
     return render_template('admin.html')
+
+# in test mode - admin panel directories - no credentials required yet
+
+
+@app.route('/admin/add/')
+def admin_add():
+    # templates word-game.html + add.questions.html body content
+    return render_template('add.question.html')
+
+# in test mode - admin panel directories - no credentials required yet
+
+
+@app.route('/admin/edit/')
+def admin_edit():
+    # templates word-game.html + edit.questions.html body content
+    return render_template('edit.questions.html')
 
 
 if __name__ == "__main__":
