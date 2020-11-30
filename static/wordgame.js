413ce6d3cc3048a3ab88a5a9ca24b3b0dd476531
Marta <ga.marta.web@gmail.com>
2020-11-30 06:14:58 +0000

    api.JS.q&a

--- static/wordgame.js
+++ static/wordgame.js
@@ -23,6 +23,7 @@ request.onload = function () {
 }
 request.send();
 console.log(phaslo)
+console.log(phaslo[0])
 var numer = Math.floor(Math.random() * phaslo.length) - 1;
 numer++; if (numer > phaslo.length) numer = 0;
 
