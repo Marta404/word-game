094503fe9159e47ce8f4cfe58457e55cd1a24ec7
Marta <ga.marta.web@gmail.com>
2020-12-03 04:19:03 +0000

    login feature -m

--- game.py
+++ game.py
@@ -8,7 +8,6 @@ from datetime import datetime
 # new to use login
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 
 
 app = Flask(__name__)
