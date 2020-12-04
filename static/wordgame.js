684f9740e7b6fdfc1827b31b985ded7e73407beb
Marta <ga.marta.web@gmail.com>
2020-12-04 05:23:15 +0000

    bgcolch

--- static/wordgame.js
+++ static/wordgame.js
@@ -171,7 +171,7 @@ request.onload = function () {
 		//$(".basic").on("click", notify);
 		function notify(event) {
 			alert("clicked " + event.data.nr);
-			$("#w" + event.data.nr).css('background', 'green');
+			$("#w" + event.data.nr).css('background', 'gray');
 			//document.getElementById(bgcolch).class = "selected";
 		}
 
